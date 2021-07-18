c9d5fe90c1d0f5643ff675f710f1efbdbe10cdd5
Eri24816 <a931eric@gmail.com>
2021-07-19 03:06:34 +0800

    Create user_defined.py

---
+++ Backend/node/user_defined.py
@@ -0,0 +1,15 @@
+from .node import Component,Attribute,Port, FunctionNode, Node
+'''
+Define your own nodes here!
+
+Example:
+
+class IsEvenNode(FunctionNode):
+    display_name = 'is_even'
+    in_names = ['n']
+    out_names = ['out']
+    
+    @staticmethod
+    def function(n):
+        return not (n%2)
+'''
